4a048a712cca3af6bb825803c88189d2a9c4d226
Lucien <89356326+Ala-R-F@users.noreply.github.com>
2024-09-27 10:47:24 -0300

    Update ex1.cpp

--- Semestre 4/POO - C++/lista01/ex1.cpp
+++ Semestre 4/POO - C++/lista01/ex1.cpp
@@ -1,16 +1,10 @@
 #include <iostream>
 
-using namespace std;
 
 int main()
 {
-    int n;
-    cin >> n;
-
-    for(int i = (3 * 5 * 17 * 11 * 7); i <= n; i+= (3 * 5 * 17 * 11 * 7)){
-        cout << i << endl;
-    }
-
-
+    std::cout << "Hello world!";
     return 0;
 }
+
+
